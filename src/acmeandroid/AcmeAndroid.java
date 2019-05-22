703e1d44a39b8078fe88d9dbdb443cb2ce2ed518
Wellington Regis <wellington.regis@gmail.com>
2019-05-22 12:00:20 +0100

    Update AcmeAndroid.java

--- src/acmeandroid/AcmeAndroid.java
+++ src/acmeandroid/AcmeAndroid.java
@@ -15,7 +15,13 @@ public class AcmeAndroid {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Android android = new Android();
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                Android android = new Android();
+            }
+        };
+        thread.start();  
     }
-    
+
 }
